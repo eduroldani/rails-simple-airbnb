5265c5c84a35a001b0dbfda66893c477026a5ceb
eduardo roldan <eduroldanisaac@gmail.com>
2022-04-15 19:27:55 +0200

    middle

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,45 @@
+class FlatsController < ApplicationController
+
+  def index
+    @flats = Flat.all
+  end
+
+  def flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "flats", to: "flats#index"
+
+  get "flats/:id", to: "flats#flat", as: :flat
+
+  get "new", to: "flats#new",  as: :new_flat
+  post "flats", to: "flats#create"
+
+  get "flats/:id/edit", to: "flats#edit", as: :edit
+  patch "flats/:id", to: "flats#update"
+
+  delete "flats/:id", to: "flats#destroy"
+
 end
